1d3edb68b96a537001fcb8fbaf1f65b12f46c21a
Waifu Forever <itachiut1r4@gmail.com>
2022-05-26 20:36:11 -0300

    update: left header area

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,8 @@ type Props = {};
 
 const Header: NextPage<Props> = () => {
     return (
-        <header>
-            <div className="relative flex items-center h-10">
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+            <div className="relative flex items-center h-10 cursor-pointer my-auto">
                 <Image
                     src="https://links.papareact.com/qd3"
                     layout="fill"
