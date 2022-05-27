d101093d3f3587ba2ce8f28620908197702da609
Waifu Forever <itachiut1r4@gmail.com>
2022-05-27 16:45:25 -0300

    add: date picker

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,10 +7,35 @@ import {
     UsersIcon,
 } from '@heroicons/react/solid';
 import Image from 'next/image';
+import { useState } from 'react';
+import { DateRangePicker, RangeKeyDict } from 'react-date-range';
+
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 
 type Props = {};
+type selectionRange = {
+    startDate: Date;
+    endDate: Date;
+    key: string;
+};
 
 const Header: NextPage<Props> = () => {
+    const [searchInput, setSearchInput] = useState('');
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
+    const selectionRange: selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection',
+    };
+
+    const handleSelect = (ranges: RangeKeyDict) => {
+        setStartDate(ranges.selection.startDate!);
+        setEndDate(ranges.selection.endDate!);
+    };
+
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
             <div className="relative flex items-center h-10 cursor-pointer my-auto">
@@ -27,17 +52,30 @@ const Header: NextPage<Props> = () => {
                     className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeolder-gray-400"
                     type="text"
                     placeholder="Start your search"
+                    value={searchInput}
+                    onChange={(e) => setSearchInput(e.target.value)}
                 />
                 <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
             </div>
-            <div className='flex items-center space-x-4 justify-end text-gray-500'>
-                <p className='hidden md:inline cursor-pointer'>Become a host</p>
-                <GlobeAltIcon className='h-6'/>
-                <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
-                    <MenuIcon className='h-6'/>
-                    <UserCircleIcon className='h-6'/>
+            <div className="flex items-center space-x-4 justify-end text-gray-500">
+                <p className="hidden md:inline cursor-pointer">Become a host</p>
+                <GlobeAltIcon className="h-6" />
+                <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+                    <MenuIcon className="h-6" />
+                    <UserCircleIcon className="h-6" />
                 </div>
             </div>
+            {searchInput && (
+                <div>
+                    <DateRangePicker
+                        ranges={[selectionRange]}
+                        minDate={new Date()}
+                        rangeColors={['#FD5861']}
+                        onChange={handleSelect}
+                    />
+                    <h1>{searchInput}</h1>
+                </div>
+            )}
         </header>
     );
 };
