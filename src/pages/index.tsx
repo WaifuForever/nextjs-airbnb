cd520e6881057686735017f24d7151feb3122657
Waifu Forever <itachiut1r4@gmail.com>
2022-05-27 14:40:39 -0300

    add: Explore Nearby section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,17 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
+import SmallCard from '../components/SmallCard';
 
-const Home: NextPage = () => {
+type Props = {
+    exploreData: {
+        location: string;
+        distance: string;
+        img: string;
+    }[];
+};
+
+const Home: NextPage<Props> = ({ exploreData }) => {
     return (
         <div className="">
             <Head>
@@ -12,8 +21,37 @@ const Home: NextPage = () => {
             </Head>
             <Header />
             <Banner />
+
+            <main className="max-w-7xl mx-auto px-8 sm:px-16">
+                <section className="pt-6">
+                    <h2 className="text-4xl font-semibold pb-5">
+                        Explore Nearby
+                    </h2>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                        {exploreData?.map((item, i) => (
+                            <SmallCard
+                                key={item.img + i}
+                                img={item.img}
+                                distance={item.distance}
+                                location={item.location}
+                            />
+                        ))}
+                    </div>
+                </section>
+            </main>
         </div>
     );
 };
+
+export async function getStaticProps() {
+    const exploreData = await fetch('https://links.papareact.com/pyp').then(
+        (res) => res.json()
+    );
+    return {
+        props: {
+            exploreData,
+        },
+    };
+}
 
 export default Home;
